cbbc04f37ee36f498be8e24958715e9ccdc09d28
Alejandro Gallo <aamsgallo@gmail.com>
2021-11-11 20:59:18 +0100

    Add mathjax config

---
+++ mathjax.config.js
@@ -0,0 +1,8 @@
+window.MathJax = {
+  tex2jax: {
+    inlineMath: [['$', '$'], ['\(', '\)']],
+    displayMath: [['$$', '$$'], ['\[', '\]']],
+    processEscapes: true
+  },
+  TeX: {extensions: ['autoload-all.js']}
+};
