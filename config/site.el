3b353ca4f3d660d74adb518202c8ef28b5a02d5a
Alejandro Gallo <aamsgallo@gmail.com>
2021-11-11 22:55:41 +0100

    Update site.el

--- config/site.el
+++ config/site.el
@@ -2,9 +2,9 @@
 
 (setq package-enable-at-startup t)
 (setq package-archives
-  '(("gnu"   . "http://elpa.gnu.org/packages/")
-    ("melpa" . "http://melpa.org/packages/"   )
-    ("org"   . "http://orgmode.org/elpa/"     )))
+      '(("gnu"   . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/"   )
+        ("org"   . "http://orgmode.org/elpa/"     )))
 (message "Initializing packages")
 (package-initialize)
 (message "done")
@@ -16,22 +16,19 @@
 (eval-when-compile
   (require 'use-package))
 
-;(add-to-list 'load-path "./lisp/external/htmlize")
-;(require 'htmlize)
-
 (use-package htmlize
-             :ensure t)
+  :ensure t)
 
 (use-package raku-mode
-             :ensure t)
+  :ensure t)
 
 (require 'org)
 
 (use-package org-plus-contrib
-             :ensure t)
+  :ensure t)
 
 (use-package org-src
-             :ensure t)
+  :ensure t)
 
 (use-package org-src
   :config
@@ -45,11 +42,10 @@
   (interactive)
   (require 'cl-macs)
   (cl-macrolet ((cc4s/log (fmt &rest args)
-                          `(message
-                            (format ,(concat "\x1b[35m∀ "
-                                             fmt
-                                             "\x1b[0m")
-                                    ,@args))))
+                          `(message (format ,(concat "\x1b[35m∀ "
+                                                     fmt
+                                                     "\x1b[0m")
+                                            ,@args))))
     (let* ((site-file-path (file-name-directory (buffer-file-name)))
            (publish-directory (format "%s/build/" site-file-path))
            (org-publish-project-alist
