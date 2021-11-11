dcc67de8fa73cb0025b1c6978b9bdab2952c301b
Alejandro Gallo <aamsgallo@gmail.com>
2021-11-12 00:29:02 +0100

    Add navigation

--- config/site.el
+++ config/site.el
@@ -36,6 +36,58 @@
 
 (load-theme 'tsdh-light)
 
+(defvar cc4s/html-head-libs
+  "
+    <meta charset='UTF-8'>
+    <meta name='apple-mobile-web-app-capable' content='yes'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link
+      href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css'
+      rel='stylesheet'
+      integrity='sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU'
+      crossorigin='anonymous'>
+    <script
+      src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js'
+      integrity='sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ'
+      crossorigin='anonymous'></script>
+  ")
+
+(defvar cc4s/navigation-bar
+  "
+  <nav class='navbar navbar-expand-lg navbar-light bg-light'>
+    <div class='container-fluid'>
+      <a class='navbar-brand' href='#'>
+        Cc4s
+      </a>
+      <button class='navbar-toggler' type='button'
+              data-bs-toggle='collapse'
+              data-bs-target='#navbarSupportedContent'
+              aria-controls='navbarSupportedContent'
+              aria-expanded='false'
+              aria-label='Toggle navigation'>
+        <span class='navbar-toggler-icon'></span>
+      </button>
+      <div class='collapse navbar-collapse' id='navbarSupportedContent'>
+        <ul class='navbar-nav me-auto mb-2 mb-lg-0'>
+          <li class='nav-item'>
+            <a class='nav-link' aria-current='page' href='/index.html'>
+              <i class='fa fa-home'></i>
+              Home
+            </a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='/sitemap.html'>
+              <i class='fa fa-sign-out'></i>
+              Sitemap
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+ ")
+
+
 (defun cc4s/publish-site ()
   (interactive)
   (require 'cl-macs)
@@ -56,12 +108,14 @@
                ;;:publishing-function org-html-export-to-html
                :htmlized-source nil
                :html-validation-link nil
+               :html-head-extra ,cc4s/html-head-libs
                :language en
-               :html-use-infojs t
+               ;:html-use-infojs nil
+               ;:html-link-home "sitemap.html"
                :auto-sitemap t
+               :html-preamble ,cc4s/navigation-bar
                :sitemap-title "Sitemap"
-               :html-link-home "sitemap.html"
-               :exclude ".*templates.*"
+               :exclude "config/*"
                :recursive t))))
       (gallo/log "Publishing site")
       (gallo/log "build directory %s" publish-directory)
