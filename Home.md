8ca75aa5d1cba74e9b120d0a84eed028e7d8ed5d
Alejandro Gallo <aamsgallo@gmail.com>
2021-11-11 22:47:21 +0100

    Add some emacs configuration

--- Home.md
+++
@@ -1,9 +0,0 @@
-# CC4S User Documentation
-
-This wiki contains useful information to
-use the [`cc4s`](https://cc4s.org) code.
-
-## Writing documentation
-
-If you are writing documentation for `cc4s`,
-read the [documentation writing manual](how-to-write.org).
---
+++ config/site.el
@@ -0,0 +1,69 @@
+(require 'package)
+
+(setq package-enable-at-startup t)
+(setq package-archives
+  '(("gnu"   . "http://elpa.gnu.org/packages/")
+    ("melpa" . "http://melpa.org/packages/"   )
+    ("org"   . "http://orgmode.org/elpa/"     )))
+(message "Initializing packages")
+(package-initialize)
+(message "done")
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;(add-to-list 'load-path "./lisp/external/htmlize")
+;(require 'htmlize)
+
+(use-package htmlize
+             :ensure t)
+
+(use-package raku-mode
+             :ensure t)
+
+(require 'org)
+
+(use-package org-plus-contrib
+             :ensure t)
+
+(use-package org-src
+             :ensure t)
+
+(use-package org-src
+  :config
+  (setq org-src-fontify-natively t
+        org-src-preserve-indentation t
+        org-src-tab-acts-natively t))
+
+(load-theme 'tsdh-light)
+
+(defun cc4s/publish-site ()
+  (interactive)
+  (require 'cl-macs)
+  (cl-macrolet ((cc4s/log (fmt &rest args)
+                          `(message
+                            (format ,(concat "\x1b[35m∀ "
+                                             fmt
+                                             "\x1b[0m")
+                                    ,@args))))
+    (let* ((site-file-path (file-name-directory (buffer-file-name)))
+           (publish-directory (format "%s/build/" site-file-path))
+           (org-publish-project-alist
+            `(("site"
+               :base-directory ,site-file-path
+               :publishing-directory ,publish-directory
+               :section-numbers nil
+               :table-of-contents t
+               :publishing-function org-html-publish-to-html
+               ;;:publishing-function org-html-export-to-html
+               :htmlized-source nil
+               :language en
+               :exclude ".*templates.*"
+               :recursive t))))
+      (cc4s/log "Publishing site")
+      (cc4s/log "build directory %s" publish-directory)
+      (org-publish-current-project))))
