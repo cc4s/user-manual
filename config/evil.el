1ae7e4ec3ceb80b6d701ea6502307a680ca3637e
Alejandro Gallo <aamsgallo@gmail.com>
2021-12-03 14:42:47 +0100

    Fix evil mode

--- config/evil.el
+++ config/evil.el
@@ -1,3 +1,19 @@
+;; type only y or n
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; good for discovery
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode +1))
+
+;; better undo for vim
+(use-package undo-tree
+  :ensure t
+  :config
+  (global-undo-tree-mode 1))
+
+;; VIMMMMMMMMMMMMMMMMMMM ;)
 (use-package evil
   :ensure t
   :hook (after-init . evil-mode)
